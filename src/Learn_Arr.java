37f77d9bca5483797b4e845a19092ea372e1d840
ProgramKingAbel <abelmorara254@gmail.com>
2024-06-20 14:09:09 +0300

    setup external libs

---
+++ src/Learn_Arr.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Learn_Arr {
+    public static void main(String [] args) {
+        // 1. Creating the array with the long form
+
+        // declare the array
+        double[] a;
+
+        // create the array
+        a = new double[5];
+
+        for (int i = 0; i < 5; i++) {
+            a[i] = 0.0;
+        }
+
+        System.out.println(Arrays.toString(a));
+
+        // 2. Creating an array with the short form
+//        boolean[] b = new boolean[2];
+        double[] b = new double[5];
+        System.out.println(Arrays.toString(b));
+
+        // 3.
+        int[] c = {1, 2, 3, 5, 10};
+        System.out.println(Arrays.toString(c));
+
+        // a multidimensional array -> an array of one dimensional arrays
+        double[][] d = new double[3][2];
+        System.out.println(Arrays.deepToString(d));
+
+        int[][][] numbers = {{{1,2,3}, {4,5,6}, {7,8,9}}};
+        System.out.println(Arrays.deepToString(numbers));
+
+        int[][][] nums = new int[2][3][4];
+        System.out.println(Arrays.deepToString(nums));
+
+        int n = c.length;
+        for (int i = 0; i < n/2; i++) {
+            int temp = c[i];
+            c[i] = c[n-1-i];
+            c[n-i-1] = temp;
+        }
+        System.out.println(Arrays.toString(c));
+
+
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        //primitive types
+        byte age = 30;
+        short mileage = 10000;
+        // int viewsCount = 123_456_789;
+        long viewsCount = 3_123_456_789L;
+        double price = 10.99;
+        float cost = 20.00F;
+        char letter = 'A';
+        boolean isEligible = true;
+
+        // reference types
+
+        Date now = new Date();
+        System.out.println(now);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point2.x = 4;
+        System.out.println(point2.x);
+
+//        String message = new String("Hello World");
+        // Strings and escape sequences
+        String message = "Hello \"world\"" + "!!";
+        String dir_path = "c:\t\\Windows\\user\n";
+
+
+        System.out.println(message.toLowerCase());
+        System.out.println(dir_path);
+
+        // Arrays
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+
+//        System.out.println(Arrays.toString(numbers));
+
+        int[] numbers = { 2, 3, 1, 4, 5 };
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        // multidimensional arrays
+        int[][] cubes = new int[2][3];
+        cubes[0][0] = 1;
+        System.out.println(Arrays.deepToString(cubes));
+
+        int [][][] numArr = new int[2][3][4];
+        numArr[0][1][2] = 3;
+        System.out.println(Arrays.deepToString(numArr));
+
+        int [][][] numbers2 = {{{1, 2, 3}, {4, 5, 6}}};
+        System.out.println(Arrays.deepToString(numbers2));
+
+        //constants
+        final float PI = 3.14F;
+
+        double result = (double)10 / 3;
+        System.out.println(result);
+
+        //increment -> post fix and prefix
+        int x = 1;
+        System.out.println(x);
+        int y = x++; // y is 1
+//        int y = ++x; ----> y is 2
+        System.out.println(x);
+        System.out.println(y);
+
+        // implicit casting
+        // byte > short > int > long > float > double
+        // Implicit casting happens only when we dont lose data
+        short a = 1;
+        int b = 2;
+        int result2 = a + b;
+        System.out.println(result2);
+
+        // explicit casting
+        double j = 1.1;
+        int k = (int)j + 2;
+        System.out.println(k);
+
+        // wrapper classes
+        String num = "1";
+        int d = Integer.parseInt(num) + 4;
+        System.out.println(d);
+
+        // The math class
+//        int c = Math.round(1.6F);
+//        int c = (int)Math.ceil(1.1F);
+        int c = (int)Math.floor(1.9F);
+        System.out.println(c);
+
+
+
+    }
+}
